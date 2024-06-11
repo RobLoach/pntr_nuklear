1c30a2d3ec66062de485d92df146a359760526e2
Rob Loach <robloach@gmail.com>
2024-06-11 01:09:40 -0400

    Update

--- cmake/FindNuklear.cmake
+++ cmake/FindNuklear.cmake
@@ -3,8 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     Nuklear
     GIT_REPOSITORY https://github.com/Immediate-Mode-UI/Nuklear.git
-    GIT_TAG 31b5729d66403e4ca1b6ff6df2958c49f3bc2c8d
-    GIT_SHALLOW 1
+    GIT_TAG 47f570c
 )
 FetchContent_GetProperties(Nuklear)
 if (NOT Nuklear_POPULATED)
--- cmake/Findpntr_app.cmake
+++ cmake/Findpntr_app.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr_app
     GIT_REPOSITORY https://github.com/robloach/pntr_app.git
-    GIT_TAG 89de07e9798ae86b94681292d43a56e5356a662d
+    GIT_TAG e0c546e
 )
 FetchContent_MakeAvailable(pntr_app)
