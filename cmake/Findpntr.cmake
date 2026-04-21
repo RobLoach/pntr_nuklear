516c1707c26927237aad58a45761005e82503fc8
Rob Loach <robloach@gmail.com>
2026-04-21 10:00:34 -0400

    Update nuklear and pntr

--- cmake/Findpntr.cmake
+++ cmake/Findpntr.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr
     GIT_REPOSITORY https://github.com/robloach/pntr.git
-    GIT_TAG c3178af
+    GIT_TAG fb19054
 )
 FetchContent_MakeAvailable(pntr)
